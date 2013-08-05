d54079ab3a67fd5fce728b1716530a179d944898
George Henderson <georgehendersonmusic@gmail.com>
2013-08-05 16:39:06 -0500

    added nvd3

--- src/www/js/app/boot.js
+++ src/www/js/app/boot.js
@@ -14,6 +14,8 @@ require.config({
     'iScroll': 'libs/iscroll-lite-4.1.6',
     'spin': 'libs/spin',
     'jquery-spin': 'libs/jquery.spin',
+    'd3': 'libs/d3.v3',
+    'nv': 'libs/nv.d3',
     'moment': 'libs/moment',
     'lavaca': 'lavaca'
   },
@@ -32,6 +34,9 @@ require.config({
     },
     templates: {
       deps: ['dust']
+    },
+    nv: {
+      deps: ['d3']
     }
   }
 });
