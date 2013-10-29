06495b5213fd27e15b68752bc5524fe050f5077d
Jake Riesterer <jake.riesterer@mutualmobile.com>
2013-10-29 10:57:01 -0500

    More specific localStorage namespace

--- src/www/js/app/cache/localStore.js
+++ src/www/js/app/cache/localStore.js
@@ -1,5 +1,5 @@
 define(function(require) {
 	var LocalStore = require('lavaca/storage/LocalStore');
-  var localStore = new LocalStore('clover'); // change 'lavaca_app' to your namespace
+  var localStore = new LocalStore('cloverReporting');
 	return localStore;
 });
