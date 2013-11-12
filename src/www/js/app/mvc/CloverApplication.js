94a267d127c943928c419b3316e691b03b6cb0e9
Jake Riesterer <jake.riesterer@mutualmobile.com>
2013-11-12 12:28:06 -0600

    Switch back to a custom onTapLink and limit ‘[href]’ selector to only anchors

--- src/www/js/app/mvc/CloverApplication.js
+++ src/www/js/app/mvc/CloverApplication.js
@@ -5,6 +5,24 @@ define(function(require) {
   var CloverApplication = Application.extend(function CloverApplication() {
     Application.apply(this, arguments);
   }, {
+    onTapLink: function(e) {
+      var link = $(e.currentTarget),
+          defaultPrevented = e.isDefaultPrevented(),
+          url = link.attr('href') || link.attr('data-href'),
+          isExternal = link.is('[data-external]') || _isExternal(url);
+
+      if (!defaultPrevented) {
+        if (isExternal) {
+          location.href = url;
+        } else {
+          e.preventDefault();
+          if (url) {
+            url = url.replace(/^\/?#/, '');
+            this.router.exec(url);
+          }
+        }
+      }
+    },
     bindLinkHandler: function() {
       var $body = $(document.body),
           type = 'click';
@@ -14,13 +32,28 @@ define(function(require) {
         $body.on('click', 'a', _stopEvent);
       }
       $body
-        .on(type, '[href], [data-href]', this.onTapLink.bind(this));
+        .on(type, 'a[href], [data-href]', this.onTapLink.bind(this));
     }
   });
 
   function _stopEvent(e) {
     e.preventDefault();
     e.stopPropagation();
+  }
+
+  function _isExternal(url) {
+    var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
+    if (typeof match[1] === 'string'
+        && match[1].length > 0
+        && match[1].toLowerCase() !== location.protocol) {
+      return true;
+    }
+    if (typeof match[2] === 'string'
+        && match[2].length > 0
+        && match[2].replace(new RegExp(':('+{'http:':80,'https:':443}[location.protocol]+')?$'), '') !== location.host) {
+      return true;
+    }
+    return false;
   }
 
   return CloverApplication;
