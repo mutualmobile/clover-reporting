09fcbcb313d6fa11f4f9206a8d8a162f2e68149d
Jake Riesterer <jake.riesterer@mutualmobile.com>
2013-11-11 18:52:48 -0600

    Update lavaca and switch to default onTapLink

--- src/www/js/app/mvc/CloverApplication.js
+++ src/www/js/app/mvc/CloverApplication.js
@@ -1,44 +1,26 @@
 define(function(require) {
   var Application = require('lavaca/mvc/Application'),
-      History = require('lavaca/net/History'),
       $ = require('$');
 
   var CloverApplication = Application.extend(function CloverApplication() {
     Application.apply(this, arguments);
   }, {
-    // Override default link handling behavior
-    onTapLink: function(e) {
-      var link = $(e.currentTarget),
-          defaultPrevented = e.isDefaultPrevented(),
-          url = link.attr('href') || link.attr('data-href'),
-          rel = link.attr('rel'),
-          target = link.attr('target'),
-          isExternal = link.is('[data-external]') || _isExternal(url);
-
-      if (!defaultPrevented) {
-        if (target === '_blank' || isExternal) {
-          return true;
-        } else {
-          e.preventDefault();
-          if (rel === 'back') {
-            History.back();
-          } else if (rel === 'cancel') {
-            this.viewManager.dismiss(e.currentTarget);
-          } else if (url) {
-            url = url.replace(/^\/?#/, '');
-            this.router.exec(url).error(this.onInvalidRoute);
-          }
-        }
+    bindLinkHandler: function() {
+      var $body = $(document.body),
+          type = 'click';
+      if ($body.hammer) {
+        $body = $body.hammer();
+        type = 'tap';
+        $body.on('click', 'a', _stopEvent);
       }
+      $body
+        .on(type, '[href], [data-href]', this.onTapLink.bind(this));
     }
   });
 
-  // http://stackoverflow.com/questions/6238351/fastest-way-to-detect-external-urls
-  function _isExternal(url) {
-    var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
-    if (typeof match[1] === 'string' && match[1].length > 0 && match[1].toLowerCase() !== location.protocol) return true;
-    if (typeof match[2] === 'string' && match[2].length > 0 && match[2].replace(new RegExp(':('+{'http:':80,'https:':443}[location.protocol]+')?$'), '') !== location.host) return true;
-    return false;
+  function _stopEvent(e) {
+    e.preventDefault();
+    e.stopPropagation();
   }
 
   return CloverApplication;
